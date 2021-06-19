67067f17380ddd5afffe39e26c082e58b05e3a13
junghwankim <b6nqctxwfs@privaterelay.appleid.com>
2021-06-19 22:17:25 +0900

    구글 창 따라해보기

---
+++ google active.js
@@ -0,0 +1,9 @@
+function move1() {
+  location.href = "https://www.google.com/doodles";
+}
+function effect1() {
+  document.getElementById("box1").style["boxshadow"] = "10px 5px 5px gray";
+}
+function move2() {
+  location.href = "https://www.google.com/webhp";
+}
